bfb8d6a6489fb941971679854b886017ad1e3ca2
Camilo sanchez <casj4144@gmail.com>
2024-08-01 22:33:37 -0500

    Cambios Computer-web

--- Computer-Web/js/addbook.js
+++ Computer-Web/js/addbook.js
@@ -1,41 +1,46 @@
-function addbook() {
-    let codeBook = document.getElementById(intup1).value;
-    let nameBook = document.getElementById(input2).value;
-    let authorBook = document.getElementById(input3).value;
-    let genreBook = document.getElementById(input4).value;
-    let yearBook = document.getElementById(input7).value;
-    let pageQuantityBook = document.getElementById(input5).value;
-    let publisherBook = document.getElementById(input6).value;
+function addBook(){
+    let codeBook = document.getElementById(`input-code-book`).value;
+    let nameBook = document.getElementById(`input-name-book`).value;
+    let authorBook = document.getElementById(`input-author-book`).value;
+    let genreBook = document.getElementById(`input-genre-book`).value;
+    let yearPublish = document.getElementById(`input-year-publish`).value;
+    let pageQuantity = document.getElementById(`input-page-quantity`).value;
+    let publisherBook = document.getElementById(`input-publisher-book`).value;
 
-    let bookdta = {
+    let bookData = {
         code: codeBook,
         name: nameBook,
         author: authorBook,
         genre: genreBook,
-        yearPublish: yearBook,
-        ppageQuantity: pageQuantityBook,
+        yearPublish: yearPublish,
+        pageQuantity: pageQuantity,
         publisher: publisherBook
     };
 
-    let url = "http://localhost:8080/Library/rest/ManagementLibrary/createBook";
-    fetch(url , {
+   
+
+    
+    let url = 'http://localhost:8080/Library/rest/ManagementLibrary/createBook';
+    fetch(url, {
         method: 'POST',
         headers: {
-            "content-Type": "application/json"
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(bookdta)
+        body: JSON.stringify(bookData)
     })
     .then(response => {
-        if (!response.ok) {
-            throw new Error("Ocurrio un error " + response.status);
-    }
-    return response.json();
-})
-.then(data => {
-    alert("Se agrego el registro");
-    window.location.href = "./dashboard.html";
-})
-.catch(error => {
-    console.error("Ocurrio el siguiente error con la operación: ", error);
-});
+        if(!response.ok){
+            throw new Error('Ocurrió un error en la respuesta del servidor' + response)
+        }
+        return response.json();
+    })
+    .then (data =>{
+        alert("Se agregó el registro.");
+        window.location.href = "./dashboard.html";
+    
+    })
+    .catch(error =>{
+        console.error('Ocurrio un error con la operacion ', error);
+    })
 }
+
